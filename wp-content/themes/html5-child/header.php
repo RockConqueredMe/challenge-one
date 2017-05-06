d5fdd55826d7553a22d4237f764a67e8f1d925db
Cameron Ritchie <critchie0258@gmail.com>
2017-05-05 18:37:49 -0700

    clean up page.php

--- wp-content/themes/html5-child/header.php
+++ wp-content/themes/html5-child/header.php
@@ -16,10 +16,10 @@
 		<script>
         // conditionizr.com
         // configure environment tests
-        conditionizr.config({
-            assets: '<?php echo get_template_directory_uri(); ?>',
-            tests: {}
-        });
+        // conditionizr.config({
+        //     assets: '<?php echo get_template_directory_uri(); ?>',
+        //     tests: {}
+        // });
         </script>
 
 	</head>
@@ -32,19 +32,18 @@
 			<header class="header clear" role="banner">
 
 					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
+					<!-- <div class="logo"> -->
+						<!-- <a href="<?php echo home_url(); ?>"> -->
 							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
-					</div>
+							<!-- <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img"> -->
+						<!-- </a> -->
+					<!-- </div> -->
 					<!-- /logo -->
 
 					<!-- nav -->
-					<nav class="nav" role="navigation">
+					<!-- <nav class="nav" role="navigation">
 						<?php html5blank_nav(); ?>
-					</nav>
+					</nav> -->
 					<!-- /nav -->
-
 			</header>
 			<!-- /header -->
