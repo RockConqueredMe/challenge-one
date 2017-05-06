1b3cf91c0ed8f94b5698ae3ff3614efad4f5ae7b
RockConqueredMe <critchie0258@gmail.com>
2017-05-05 21:01:03 -0700

    Have everything except fonts and bottom four things in jumbotron (#5)
    * clean up page.php
    
    * added bootstrap suppport
    
    * added image
    
    * added center text
    
    * added fontawesome
    
    * yellow nav active
    
    * added learn more btn
    
    * fix font sizes

--- wp-content/themes/html5-child/header.php
+++ wp-content/themes/html5-child/header.php
@@ -16,35 +16,68 @@
 		<script>
         // conditionizr.com
         // configure environment tests
-        conditionizr.config({
-            assets: '<?php echo get_template_directory_uri(); ?>',
-            tests: {}
-        });
+        // conditionizr.config({
+        //     assets: '<?php echo get_template_directory_uri(); ?>',
+        //     tests: {}
+        // });
         </script>
 
 	</head>
 	<body <?php body_class(); ?>>
 
+		<!-- jumbotron -->
+		<div class="jumbotron">
+			<div class="container">
+				<nav class="navbar navbar-toggleable-md">
+					<a href="#" class="navbar-brand">MoGo</a>
+					<div class="collpase navbar-collapse">
+						<ul class="navbar-nav ml-auto">
+				      <li class="nav-item active">
+				        <a class="nav-link" href="#">ABOUT <span class="sr-only">(current)</span></a>
+				      </li>
+				      <li class="nav-item">
+				        <a class="nav-link" href="#">SERVICE</a>
+				      </li>
+				      <li class="nav-item">
+				        <a class="nav-link" href="#">WORK</a>
+				      </li>
+							<li class="nav-item">
+				        <a class="nav-link" href="#">BLOG</a>
+				      </li>
+							<li class="nav-item">
+				        <a class="nav-link" href="#">CONTACT</a>
+				      </li>
+							<li class="nav-item">
+				        <a class="nav-link" href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+				      </li>
+							<li class="nav-item">
+				        <a class="nav-link" href="#"><i class="fa fa-search" aria-hidden="true"></i></i></a>
+				      </li>
+	    			</ul>
+					</div>
+				</nav>
+				<div class="overjumbo">
+					<h2>Creative Template</h2>
+					<h1>WELCOME TO MOGO</h1>
+					<hr class="jumbohr">
+					<div class="jumbobtn">
+						<p>LEARN MORE</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- end jumbotron -->
 		<!-- wrapper -->
 		<div class="wrapper">
 
 			<!-- header -->
 			<header class="header clear" role="banner">
 
-					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
-						</a>
-					</div>
-					<!-- /logo -->
 
 					<!-- nav -->
-					<nav class="nav" role="navigation">
+					<!-- <nav class="nav" role="navigation">
 						<?php html5blank_nav(); ?>
-					</nav>
+					</nav> -->
 					<!-- /nav -->
-
 			</header>
 			<!-- /header -->
--- wp-content/themes/html5-child/page.php
+++ wp-content/themes/html5-child/page.php
@@ -4,7 +4,7 @@
 		<!-- section -->
 		<section>
 
-			<h1><?php the_title(); ?></h1>
+			<!-- <h1><?php the_title(); ?></h1> -->
 
 		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
@@ -13,11 +13,9 @@
 
 				<?php the_content(); ?>
 
-				<?php comments_template( '', true ); // Remove if you don't want comments ?>
-
 				<br class="clear">
 
-				<?php edit_post_link(); ?>
+				<!-- <?php edit_post_link(); ?> -->
 
 			</article>
 			<!-- /article -->
@@ -26,20 +24,10 @@
 
 		<?php else: ?>
 
-			<!-- article -->
-			<article>
-
-				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-
-			</article>
-			<!-- /article -->
-
 		<?php endif; ?>
 
 		</section>
 		<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
